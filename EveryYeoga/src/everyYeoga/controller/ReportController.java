d83bf36c3ea44f36a7a663da5004d13e441fcbba
INEA <INEA@192.168.0.21>
2017-11-25 14:19:36 +0900

    2017.11.25 UserController 수정중 인애

--- EveryYeoga/src/everyYeoga/controller/ReportController.java
+++ EveryYeoga/src/everyYeoga/controller/ReportController.java
@@ -1,5 +1,94 @@
 package everyYeoga.controller;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import everyYeoga.domain.Report;
+import everyYeoga.domain.User;
+import everyYeoga.service.ReportService;
+import everyYeoga.service.UserService;
+
+
+@Controller
+@RequestMapping("report")     	        // 인애
 public class ReportController {
 
+	@Autowired
+	private ReportService reportService;
+	private UserService userService;
+	
+	
+	@RequestMapping(value = "/regist.do", method = RequestMethod.GET)
+	public String registReport(HttpServletRequest req, String classifyReport, String classifyId, String userId, Model model) {
+		
+		HttpSession session = req.getSession();
+		if(session == null || session.getAttribute("xxxxxxxxxxx") == null) {     // 저장한 이름 넣기
+			return "redirect:login";  
+		}
+		
+		User user = (User)session.getAttribute("xxxxxxxxxxxx");    // 저장한 이름 넣기
+		model.addAttribute("user", user);
+		model.addAttribute("classifyReport", classifyReport);
+		
+		return "redirect:report";  /* 신고 작성 페이지로 이동 */
+	}
+	
+	@RequestMapping(value = "/regist.do", method = RequestMethod.POST)
+	public String registReport(Report report) {
+		
+		Date today = new Date(Calendar.getInstance().getTimeInMillis());
+		
+		report.setRegDate(today);
+		report.setReportUser(userService.searchByUserId(""));
+		report.setReportedUser(userService.searchByUserId(""));
+		
+		reportService.registReport(report, "classifyId");
+		
+		return "redirect:groupMain";  /* 모임 메인 */
+	}	
+	
+	@RequestMapping("searchArticle.do") /* 인애 - 2017.11.25 article search 파라미터 String groupId 뺌  */
+	public ModelAndView searchArticleReport(@RequestParam("articleId")String reportedArticleId) { //jsp에서 넘어오는  값의이름이 articleId (?)
+
+		Report articleReportDetail = reportService.searchArticleReport(reportedArticleId);
+	
+		ModelAndView modelAndView = new ModelAndView("reportDetail");    //  report detail (상세 신고내역) 나오는 페이지 이름
+		modelAndView.addObject("articleReport", articleReportDetail);
+		
+		return modelAndView;
+	}
+	
+	/*   ↑ 신고 상세내역 보는 곳 ↓  */
+	
+	@RequestMapping("searchComment.do")
+	public ModelAndView searchCommentReport(@RequestParam("commentId")String reportedCommentId) {  //jsp에서 넘어오는  값의이름이 commentId (?)
+		
+		Report commentReportDetail = reportService.searchCommentReport(reportedCommentId);
+		
+		ModelAndView modelAndView = new ModelAndView("reportDetail");    // report detail (상세 신고내역) 나오는 페이지 이름
+		modelAndView.addObject("commentReport", commentReportDetail);
+		
+		return modelAndView;
+	}
+	@RequestMapping("searchAll.do")
+	public ModelAndView searchReportList() {
+	
+		List<Report> list = reportService.searchAllReport();
+		ModelAndView modelAndView = new ModelAndView("adminPage"); // 모든 신고내역 보는곳 : 관리자페이지
+		modelAndView.addObject("reportList", list);
+		
+		return modelAndView;
+	}
 }
--- EveryYeoga/src/everyYeoga/controller/UserController.java
+++ EveryYeoga/src/everyYeoga/controller/UserController.java
@@ -1,5 +1,110 @@
 package everyYeoga.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import everyYeoga.domain.User;
+import everyYeoga.service.UserService;
+
+@Controller
+@RequestMapping("user")
 public class UserController {
+
+	@Autowired
+	UserService userService;
+
+	@RequestMapping("regist.do")
+	public String showRegistUser(User user) {
+		return "redirect:registUser";
+	}
+
+	@RequestMapping(value = "regist.do", method = RequestMethod.POST)
+	public String registUser(User user) {
+
+		boolean registed = userService.registUser(user);
+		if (!registed) {
+			return "redirect:login.do";
+		}
+		return "redirect:main";
+	}
+
+	@RequestMapping("detail.do")
+	public ModelAndView searchMyPage(HttpServletRequest req) {   // 마이페이지 누르면 회원정보 나옴
+		
+		HttpSession session = req.getSession();
+		User user = userService.searchByUserId(session.getId());
+		
+		ModelAndView modelAndView = new ModelAndView("myPage");
+		modelAndView.addObject("user", user);
+		
+		return modelAndView;
+	}
+
+	@RequestMapping("modify.do")
+	public String modifyUser(HttpServletRequest req) {   // 2017.11.25 파라미터 User user 에서 req 로 변경
+
+		HttpSession session = req.getSession();
+		User user = userService.searchByUserId(session.getId());
+		
+		userService.modifyUser(user);
+		return "redirect:myPage";
+	}
+	
+	
+	@RequestMapping(value="modify.do", method=RequestMethod.POST)
+	public ModelAndView showModifyPage(User user) {  // 문서에는 get 과 post 로 안나눠져있음!
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("modifyUser");
+		
+		return modelAndView;
+	}
+
+	@RequestMapping("remove.do")
+	public String removeUser(String userId) {
+		userService.removeUser(userId);
+		return "redirect:login";
+	}
+
+	@RequestMapping("login.do")
+	public String showLogin(User user) {
+		return "redirect:login";
+	}
+
+	@RequestMapping(value = "login.do", method = RequestMethod.POST)
+	public String login(User user, HttpServletRequest req) {
+		
+//		User loginedUser = userService.
+//		
+//		if(loginedUser != null) {
+//			HttpSession session = req.getSession();
+//			session.setAttribute("loginedUser", loginedUser);
+//		}else {
+//			HttpSession session = req.getSession();
+//			session.invalidate();
+//		}
+		return "redirect:main";
+
+	}
+
+	@RequestMapping("logout.do")
+	public String logout(HttpServletRequest req) {
+
+		HttpSession session = req.getSession();
+		session.invalidate();
+		return "redirect:login.do";
+	}
+
+	@RequestMapping("")
+	public String searchBlockedUser(User user) {
+
+		return "";
+	}
 
 }
